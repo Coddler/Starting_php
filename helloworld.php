7a8eafc81da4cc4dc144326802f0471535776834
Coddler <irandjelovic8@gmail.com>
2015-10-13 00:30:31 +0200

    helloworld.php
    first code in PHP.

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+
+<? php
+
+echo 'Hello world!<br>';
+
+$firstnumber=5;
+for ($i =0 ; $i<5; $i++) $firstnumber=$firstnumber+1;
+echo "what is 1+1+1+1+1+5 equal to? <br>";
+echo "solution is: $firstnumber";
+
+
+?>
